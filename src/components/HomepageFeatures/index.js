429b969c4718bbb06682728b4325fe0da1c3d3d7
Galen Lim <jingliang.lim@gmail.com>
2023-03-11 22:16:07 +0800

    content: add image attribution and style

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,40 +1,28 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Learn',
-    Svg: require('@site/static/img/learn.svg').default,
-    description: (
-      <>
-        Interesting stuff.
-      </>
-    ),
+    title: "Learn",
+    Svg: require("@site/static/img/learn.svg").default,
+    description: <>Interesting stuff.</>,
   },
   {
-    title: 'Play',
-    Svg: require('@site/static/img/play.svg').default,
-    description: (
-      <>
-        And have fun!
-      </>
-    ),
+    title: "Play",
+    Svg: require("@site/static/img/play.svg").default,
+    description: <>And have fun!</>,
   },
   {
-    title: 'Document',
-    Svg: require('@site/static/img/document.svg').default,
-    description: (
-      <>
-        To consolidate.
-      </>
-    ),
+    title: "Document",
+    Svg: require("@site/static/img/document.svg").default,
+    description: <>To consolidate.</>,
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
@@ -54,6 +42,23 @@ export default function HomepageFeatures() {
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
+        </div>
+        <div className="row">
+          <div className="col text--center">
+            <span className={styles.attribution}>
+              Vectors and icons by{" "}
+              <a
+                href="https://dribbble.com/thecarolizer?ref=svgrepo.com"
+                target="_blank"
+              >
+                Carol Liao
+              </a>{" "}
+              in CC Attribution License via{" "}
+              <a href="https://www.svgrepo.com/" target="_blank">
+                SVG Repo
+              </a>
+            </span>
+          </div>
         </div>
       </div>
     </section>
